32b446cb6cd826ae2b7a88c61eb6db1bbb042b85
Philipp Liegl <philipp.liegl@ecosio.com>
2015-04-20 15:00:20 +0200

    ADDED first version of ebinterface web

---
+++ ebinterface-core/src/main/java/at/ebinterface/validation/validator/jaxb/package-info.java
@@ -0,0 +1,1 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://validation.erpel.at", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED) package at.ebinterface.validation.validator.jaxb;
