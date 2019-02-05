7c4c5aacb3aaf9d8c636ed1e69b61d36c78c3590
Robert Gründler <robert@dubture.com>
2019-02-05 11:02:09 +0100

    UPDATED build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#mvn clean package
+mvn clean package -DskipTests=true
 docker build -t ebinterface/validator-web .
 docker tag validator-web:latest ebinterface/validator-web:latest
 docker push ebinterface/validator-web:latest
