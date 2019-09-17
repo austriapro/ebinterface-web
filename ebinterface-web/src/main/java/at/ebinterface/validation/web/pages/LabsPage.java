37b43f8ebb66520691bba66e72ebc608beb77aa6
Philip Helger <philip@helger.com>
2019-09-17 15:22:49 +0200

    XRechnung to eBinterface

--- ebinterface-web/src/main/java/at/ebinterface/validation/web/pages/LabsPage.java
+++ ebinterface-web/src/main/java/at/ebinterface/validation/web/pages/LabsPage.java
@@ -1,29 +1,36 @@
 package at.ebinterface.validation.web.pages;
 
-public class LabsPage extends BasePage {
-
+public final class LabsPage extends BasePage
+{
   @Override
-  protected void onInitialize() {
-    super.onInitialize();
+  protected void onInitialize ()
+  {
+    super.onInitialize ();
 
-    //Add the input form
-    final LabsForm inputForm = new LabsForm("inputForm");
-    add(inputForm);
+    final boolean bIsStartPage = false;
 
-    //Add the form for showing the supported rules
-    final ShowRulesForm showRulesForm = new ShowRulesForm("showRulesForm");
-    add(showRulesForm);
+    // Add the input form
+    final LabsForm inputForm = new LabsForm ("inputForm");
+    add (inputForm);
 
-    //Add the form for showing the supported rules
-    final UblToEbiForm ublToEbiForm = new UblToEbiForm("ublToEbiForm", false);
-    add(ublToEbiForm);
+    // Add the form for showing the supported rules
+    final ShowRulesForm showRulesForm = new ShowRulesForm ("showRulesForm");
+    add (showRulesForm);
 
-    //Add the form for convert ebInterface to UBL
-    final EbiToUblForm ebiToUblForm = new EbiToUblForm("ebiToUblForm", false);
-    add(ebiToUblForm);
+    // Add the form for converting UBL to ebInterface
+    final UblToEbiForm ublToEbiForm = new UblToEbiForm ("ublToEbiForm", bIsStartPage);
+    add (ublToEbiForm);
 
-    //Add the form for convert ebInterface to XRechnung
-    final EbiToXRechnungForm ebiToXRechnungForm = new EbiToXRechnungForm("ebiToXRechnungForm", false);
-    add(ebiToXRechnungForm);
+    // Add the form for convert ebInterface to UBL
+    final EbiToUblForm ebiToUblForm = new EbiToUblForm ("ebiToUblForm", bIsStartPage);
+    add (ebiToUblForm);
+
+    // Add the form for convert XRechnung to ebInterface
+    final XRechnungToEbiForm xRechnungToEbiForm = new XRechnungToEbiForm ("xRechnungToEbiForm", bIsStartPage);
+    add (xRechnungToEbiForm);
+
+    // Add the form for convert ebInterface to XRechnung
+    final EbiToXRechnungForm ebiToXRechnungForm = new EbiToXRechnungForm ("ebiToXRechnungForm", bIsStartPage);
+    add (ebiToXRechnungForm);
   }
 }
--- ebinterface-web/src/main/java/at/ebinterface/validation/web/pages/StartPage.java
+++ ebinterface-web/src/main/java/at/ebinterface/validation/web/pages/StartPage.java
@@ -9,44 +9,54 @@ import com.helger.commons.collection.impl.CommonsArrayList;
  *
  * @author pl
  */
-public class StartPage extends BasePage {
-
+public class StartPage extends BasePage
+{
   /**
    * The possible actions
    */
-  public enum ActionType {
-    SCHEMA_VALIDATION, SCHEMA_AND_SCHEMATRON_VALIDATION, VISUALIZATION_HTML, VISUALIZATION_PDF, CONVERSION_ZUGFERD
+  public enum ActionType
+  {
+    SCHEMA_VALIDATION,
+    SCHEMA_AND_SCHEMATRON_VALIDATION,
+    VISUALIZATION_HTML,
+    VISUALIZATION_PDF,
+    CONVERSION_ZUGFERD
   }
 
-  //choices in dropdown box ZUGFeRD
-  static final List<String> ZUGFERD_LEVELS = new CommonsArrayList<> 
-                    ("ZUGFeRD (1.0) Basic", "ZUGFeRD (1.0) Comfort", "ZUGFeRD (1.0) Extended").getAsUnmodifiable ();
+  // choices in dropdown box ZUGFeRD
+  static final List <String> ZUGFERD_LEVELS = new CommonsArrayList <> ("ZUGFeRD (1.0) Basic",
+                                                                       "ZUGFeRD (1.0) Comfort",
+                                                                       "ZUGFeRD (1.0) Extended").getAsUnmodifiable ();
 
-  public StartPage() {}
-  
   @Override
   protected void onInitialize ()
   {
     super.onInitialize ();
 
-    //Add the input form
-    final InputForm inputForm = new InputForm("inputForm");
-    add(inputForm);
+    final boolean bIsStartPage = true;
 
-    //Add the form for showing the supported rules
-    final ShowRulesForm showRulesForm = new ShowRulesForm("showRulesForm");
-    add(showRulesForm);
+    // Add the input form
+    final InputForm inputForm = new InputForm ("inputForm");
+    add (inputForm);
 
-    //Add the form for convert UBL to ebInterface
-    final UblToEbiForm ublToEbiForm = new UblToEbiForm("ublToEbiForm", true);
-    add(ublToEbiForm);
+    // Add the form for showing the supported rules
+    final ShowRulesForm showRulesForm = new ShowRulesForm ("showRulesForm");
+    add (showRulesForm);
 
-    //Add the form for convert ebInterface to UBL
-    final EbiToUblForm ebiToUblForm = new EbiToUblForm("ebiToUblForm", true);
-    add(ebiToUblForm);
+    // Add the form for convert UBL to ebInterface
+    final UblToEbiForm ublToEbiForm = new UblToEbiForm ("ublToEbiForm", bIsStartPage);
+    add (ublToEbiForm);
 
-    //Add the form for convert ebInterface to XRechnung
-    final EbiToXRechnungForm ebiToXRechnungForm = new EbiToXRechnungForm("ebiToXRechnungForm", true);
-    add(ebiToXRechnungForm);
+    // Add the form for convert ebInterface to UBL
+    final EbiToUblForm ebiToUblForm = new EbiToUblForm ("ebiToUblForm", bIsStartPage);
+    add (ebiToUblForm);
+
+    // Add the form for convert XRechnung to ebInterface
+    final XRechnungToEbiForm xRechnungToEbiForm = new XRechnungToEbiForm ("xRechnungToEbiForm", bIsStartPage);
+    add (xRechnungToEbiForm);
+
+    // Add the form for convert ebInterface to XRechnung
+    final EbiToXRechnungForm ebiToXRechnungForm = new EbiToXRechnungForm ("ebiToXRechnungForm", bIsStartPage);
+    add (ebiToXRechnungForm);
   }
 }
