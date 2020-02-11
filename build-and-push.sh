d4602cae241f147a0e1f9fecb2affbb4e3711ce9
Philip Helger <philip@helger.com>
2020-02-11 19:34:38 +0100

    Added login

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,5 +2,6 @@
 
 mvn clean package -DskipTests=true
 docker build -t ebinterface/validator-web .
-docker tag validator-web:latest ebinterface/validator-web:latest
+#docker tag validator-web:latest ebinterface/validator-web:latest
+docker login -u ebinterface
 docker push ebinterface/validator-web:latest
