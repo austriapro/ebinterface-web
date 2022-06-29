8b9cb0609d394712f035c5bcc8aa22de154ccfa6
Philip Helger <philip@helger.com>
2022-06-29 09:15:16 +0200

    Improved error handling

--- docker-build-and-run.cmd
+++ docker-build-and-run.cmd
@@ -1,6 +1,18 @@
 @echo off
 
 call mvn clean package -DskipTests=true
+if errorlevel 1 goto error
+
 docker build --pull -t ebinterface/validator-web .
+if errorlevel 1 goto error
+
 docker run -d --name ebinterface-web -p 8888:8080 ebinterface/validator-web
+if errorlevel 1 goto error
+
 echo Now you can open http://localhost:8888
+goto done
+
+:error
+echo ERROR
+
+:done
